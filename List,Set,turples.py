5327df9770f966ffc55ee2af400f0da6a528f63c
brightaziat006-oss <brightaziat006@gmail.com>
2026-03-30 01:21:40 +0000

    Create List,Set,turples.py

---
+++ List,Set,turples.py
@@ -0,0 +1,3 @@
+#   List  = [] ordered and changeable. Duplicates OK
+#   Set   = {} unordered and immutable, but Add/Remove OK. NO duplicates
+#   Tuple = () ordered and unchangeable. Duplicates OK. FASTER
